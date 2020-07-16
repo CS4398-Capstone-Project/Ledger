108440fdbbc830a616671b72c25dd21df6f5d5b9
adc129 <60369328+adc129@users.noreply.github.com>
2020-07-16 13:56:51 -0500

    appointment prototype

--- backend/api/core/serializers.py
+++ backend/api/core/serializers.py
@@ -1,39 +1,62 @@
-from django.contrib.auth.models import User, Profile, User, Doctor
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import User, Profile, Patient, Doctor, Appointment
 from rest_framework import serializers
-from . import models
-from . import serializers as serializer
+from  import models
+from  import serializers as serializer
 
 
-"""
-This serializer controls how the User is returned and created when called.
-"""
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
+
         model = User
         fields = ['email', 'username', 'password', 'id']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
-            username=validated_data['username']
-        )
+        user = User(email=validated_data['email'],
+                    username=validated_data['username'])
         user.set_password(validated_data['password'])
         user.save()
         return user
 
-"""
-This serializer controls how the Profile is returned when called.
-"""
+
 class ProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
+
         model = models.Profile
-        fields = ['user', 'bio', 'location', 'birth_date', 'date_registered','phone_number', 'type']
+        fields = [
+            'user',
+            'bio',
+            'location',
+            'birth_date',
+            'date_registered',
+            'phone_number',
+            ]
+
+
+class AppointmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = models.Appointment
+        fields = [Patient, Doctor, 'date', 'time_start']
+
+
 class DoctorSerializer(serializers.ModelSerializer):
+
     class Meta:
+
         model = models.Profile
-        fields = ['doctor_name', 'registration_date', 'work']
+        fields = ['doctor_name', 'registration_date',
+                  'work'.Appointment]
+
+
 class PatientSerializer(serializers.ModelSerializer):
+
     class Meta:
-        model = models.Profile
-        fields = [Doctor, 'patient_name', 'registration_date']
+
+        model = models.Patient
+        fields = ['patient_name', 'registration_date']
