e349c8a4406eb0299632c3bf9b966f56bd566d52
Eduardo Dominguez <edoming2893@gmail.com>
2020-07-16 23:30:27 -0500

    initial commit

---
+++ frontend/original-template/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from '/mnt/c/Users/tlc/Documents/Classwork/SE/Ledger/frontend/ledger-app/src/components/Login.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
+
