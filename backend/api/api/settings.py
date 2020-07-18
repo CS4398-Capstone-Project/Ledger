0ead31792ce48e5364a0f6d7b0f91fe1f1a07940
KeondreCredit <KeondreCredit@gmail.com>
2020-07-18 18:57:52 -0500

    Integrated PostgreSQL
    Integrated PostgreSQL as the database  to be used for the project.

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'txst',
+        'PASSWORD': 'bobcats',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
