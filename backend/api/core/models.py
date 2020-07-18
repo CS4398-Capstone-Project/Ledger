7d354bee84da3131019173eb553fb404f97a7b6c
adc129 <60369328+adc129@users.noreply.github.com>
2020-07-17 23:48:55 -0500

    added profile fields

--- backend/api/core/models.py
+++ backend/api/core/models.py
@@ -5,9 +5,14 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from phonenumber_field.modelfields import PhoneNumberField
+from django_localflavor_us.models import USStateField
 
 
 # Create your models here.
+TYPE_CHOICES = (
+    ('1', 'Male'),
+    ('2', 'Female')
+)
 
 class Profile(models.Model):
 
@@ -15,6 +20,12 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     phone_number = PhoneNumberField(null=True)
+    gender = models.CharField(default="Choose Gender", max_length=50, choices=TYPE_CHOICES)
+    address_1 = models.CharField(("address"), max_length=128, default="")
+    address_2 = models.CharField(("address 2"), max_length=128, blank=True)
+    city = models.CharField(("city"), max_length=64, default="Mars")
+    state = USStateField(("state"), default="TX")
+    zip_code = models.CharField(("zip code"), max_length=5, default="43701")
 
 
 @receiver(post_save, sender=User)
@@ -33,15 +44,14 @@ def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 class Doctor(models.Model):
-
-    doctor_name = models.CharField(max_length=50)
+    profile = models.ForeignKey(Profile, null = True, on_delete=models.CASCADE)
     work = models.CharField(max_length=50)
 
 class Patient(models.Model):
 
     profile = models.ForeignKey(Profile, null = True, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    registration_date = models.DateTimeField('date-registered')
+
 
 
 class Appointment(models.Model):
