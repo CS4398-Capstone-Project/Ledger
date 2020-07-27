875f259ad5351b389df737daddc18a3aa6e3792e
TrevorLChaney <trevorchaney@gmail.com>
2020-07-27 15:02:00 -0500

    Merging divergent frontend

--- frontend/ledger-app/src/components/LoginForm.js
+++ frontend/ledger-app/src/components/LoginForm.js
@@ -1,71 +1,137 @@
 import React from "react";
-import { Link, Redirect } from "react-router-dom";
+import InputField from "./InputField";
+import SubmitButton from "./SubmitButton";
+import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 
 class LoginForm extends React.Component {
- state = {
-        credentials: {username: '', password: ''},
-        islogin: false, from: '',
-    }
-    login = event => {
-        console.log('login here');
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      buttonDisabled: false,
+    };
+  }
 
-        fetch('http://localhost:8000/auth/', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(this.state.credentials)
+  setInputValue(property, val) {
+    val = val.trim();
+    if (val.length > 12) {
+      return;
+    }
+    this.setState({
+      [property]: val,
+    });
+  }
+
+  resetForm() {
+    this.setState({
+      username: "",
+      password: "",
+      buttonDisabled: false,
+    });
+  }
+
+  async doLogin() {
+    if (!this.state.username) {
+      return;
+    }
+    if (!this.state.password) {
+      return;
+    }
+
+    this.setState({
+      buttonDisabled: true,
+    });
+
+    try {
+      let res = await fetch("127.0.0.1:8000/api/auth/token/login/", {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          "password": this.state.password,
+          "username": this.state.username,
+        }),
+      });
+/*
+    try {
+      // This will pull the login for a user that is running the backend localy
+      let res = await fetch("127.0.0.1:8000/api/auth/token/login/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          "password": this.state.password,
+          "username": this.state.username,
         })
-        .then (data => data.json())
-        .then(
-            data => {
-                localStorage.setItem('token', data) // saving token to be used by other componenets if needed
-            }
-          )
-          .then(
-              this.setState({islogin : true})
-           )
-        .catch(error => console.error(error))
+      }).then((resp) => {
+        if(resp.state === 200) {
+          this.setState({success: true});
+        } return resp.json();
+      }).then((resp) => {
+        if(this.state.success) {
+          this.props.setToken("Token " + resp.auth_token)
+        } else {
+          let str = JSON.stirgify(resp)
+          let finalMessage = str.replace(/{|},|}/g, "\n").replace(/\[|\]|/g, "").replace(/,/g, ',\n')
+          console.log(resp);
+          this.setState({errorMessage: finalMessage});
+        }
+      }).catch((error) => {
+        console.log(error, "Login try-catch failure");
+      });
+*/
+
+      let result = await res.json();
+      if (result && result.success) {
+        this.props.state.isLoggedIn = true;
+        this.props.state.username = result.username;
+        this.props.state.userID = result.id;
+        this.props.state.userPermissions = result.permission;
+      } else if (result && result.sucess === false) {
+        this.resetForm();
+        alert(result.msg);
+      }
+    } catch (e) {
+      console.log(e);
+      this.resetForm();
     }
-    inputChanged = event => {
-        const cred = this.state.credentials;
-        cred[event.target.name] = event.target.value; 
-        this.setState({credentials: cred});
-    }
+  }
 
-    renderRedirect = () => {
-      if (this.state.islogin) {
-      return <Redirect to='./Customer' /> }
-      
-    }
-    render() {
-      return (
-          <div>
-              <h1> Login user</h1>
+  async doSignUp() {}
 
-              <label>
-                  username:
-                  <input type="text" name = "username" 
-                  value={this.state.credentials.username}
-                  onChange={this.inputChanged}/>
-              </label>
-              <br/>
-              <label>
-                  password:
-                  <input type="text" name = "password" 
-                  value={this.state.credentials.password}
-                  onChange={this.inputChanged}/>
-              </label>
-              <br/>
-              <br/>
-              <button as="Loginbutton" onClick={this.login}>Login</button>
-              <br/>
-              <br/>
-              <button onClick={this.register}>Register</button>
-              {this.renderRedirect()}
-
-          </div>
-
-      );
+  render() {
+    return (
+      <div className="loginForm">
+        Login
+        <InputField
+          type="text"
+          placeholder="Username"
+          value={this.state.username ? this.state.username : ""}
+          onChange={(val) => this.setInputValue("username", val)}
+        />
+        <InputField
+          type="password"
+          placeholder="Password"
+          value={this.state.password ? this.state.username : ""}
+          onChange={(val) => this.setInputValue("password", val)}
+        />
+        <SubmitButton
+          text="Login"
+          disabled={this.state.buttonDisabled}
+          onClick={() => this.doLogin()}
+        />
+        <p>Not registered yet, Register Now</p>
+        <Link to={`/registration`}>
+          <Button text="SignUp" className='regBtn' > Register</Button>
+        </Link>
+      </div>
+    );
   }
 }
 export default LoginForm;
