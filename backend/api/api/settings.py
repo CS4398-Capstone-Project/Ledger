b64943acd4918dd9a75855a628dee0cd76f07903
KeondreCredit <KeondreCredit@gmail.com>
2020-07-29 15:53:08 -0500

    added cors
    added:
    corsheaders and CORS_ORIGIN_ALLOW_ALL = True

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'corsheaders',
     'phonenumber_field',
     'rest_framework',
     'rest_framework.authtoken',
@@ -70,7 +71,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
