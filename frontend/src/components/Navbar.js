94f9e95e71f880c3c95905f47d8e5faab73c32bf
TrevorLChaney <trevorchaney@gmail.com>
2020-07-30 14:42:00 -0500

    Rebuild of frontend branch

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <section>
+        <div className="containe2">
+          <div className="row">
+            <div className="col-sm-6">
+             
+            </div>
+            <div className="col-sm-6 logOutbtn">
+              <h1>Welcome!</h1>
+              <h2>Name:</h2>
+              <h3>Username: </h3>
+              <h3>DOB: </h3>
+              <h3>Email:</h3>
+              <h3>Phone:</h3>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+      <section></section>
+    </>
+  );
+};
+
+export default Navbar;
